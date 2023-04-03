98e195abc631ca9e4bec37b8ca99ea457534e7bb
Roman Pyzhlakov <romagifted@gmail.com>
2023-04-03 07:01:37 +0700

    changed

---
+++ core/cache/resource/web/resources/1.cache.php
@@ -0,0 +1,88 @@
+<?php  return array (
+  'resourceClass' => 'MODX\\Revolution\\modDocument',
+  'resource' => 
+  array (
+    'id' => 1,
+    'type' => 'document',
+    'pagetitle' => 'Главная',
+    'longtitle' => '«LATERES» Завод вибропрессованных изделий в Подмосковье, изделия из бетона',
+    'description' => 'Завод «LATERES» - Производство строительных, фундаментных, керамзитобетонных блоков, тротуарной плитки, бордюрного камня и облицовочного кирпича. Звоните: +7 (903) 151-3344!',
+    'alias' => 'main',
+    'link_attributes' => '',
+    'published' => 1,
+    'pub_date' => 0,
+    'unpub_date' => 0,
+    'parent' => 0,
+    'isfolder' => 1,
+    'introtext' => 'завод вибропрессованных изделий, lateres, изделия из бетона, вибропрессованные изделия',
+    'content' => '',
+    'richtext' => 1,
+    'template' => 1,
+    'menuindex' => 0,
+    'searchable' => 1,
+    'cacheable' => 1,
+    'createdby' => 1,
+    'createdon' => 1665136923,
+    'editedby' => 1,
+    'editedon' => 1676594379,
+    'deleted' => 0,
+    'deletedon' => 0,
+    'deletedby' => 0,
+    'publishedon' => 0,
+    'publishedby' => 0,
+    'menutitle' => '',
+    'donthit' => 0,
+    'privateweb' => 0,
+    'privatemgr' => 0,
+    'content_dispo' => 0,
+    'hidemenu' => 0,
+    'class_key' => 'MODX\\Revolution\\modDocument',
+    'context_key' => 'web',
+    'content_type' => 1,
+    'uri' => 'main/',
+    'uri_override' => 0,
+    'hide_children_in_tree' => 0,
+    'show_in_tree' => 1,
+    'properties' => NULL,
+    'alias_visible' => 1,
+    '_content' => '<!DOCTYPE html>
+<html lang="{$_modx->config.cultureKey}">
+    <head>
+        {include \'file:chunks/base/head.tpl\'}
+    </head>
+    <body class="bg-light">
+      
+        <div id="app"></div>
+
+        {include \'file:chunks/base/scripts.tpl\'}
+    </body>
+</html>',
+    '_isForward' => false,
+  ),
+  'contentType' => 
+  array (
+    'id' => 1,
+    'name' => 'HTML',
+    'description' => 'HTML content',
+    'mime_type' => 'text/html',
+    'file_extensions' => '',
+    'icon' => '',
+    'headers' => NULL,
+    'binary' => 0,
+  ),
+  'policyCache' => 
+  array (
+  ),
+  'sourceCache' => 
+  array (
+    'MODX\\Revolution\\modChunk' => 
+    array (
+    ),
+    'MODX\\Revolution\\modSnippet' => 
+    array (
+    ),
+    'MODX\\Revolution\\modTemplateVar' => 
+    array (
+    ),
+  ),
+);
